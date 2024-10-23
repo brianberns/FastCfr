68ad2b4c7bb6c0b8f8cc7995e01833e6c836cd46
Brian Berns <brianberns@gmail.com>
2024-10-23 10:30:34 -0400

    Added benchmark.

--- FastCfr.Test/Program.fs
+++ FastCfr.Test/Program.fs
@@ -1,17 +1,34 @@
 namespace FastCfr.Test
 
 open System
+
+open BenchmarkDotNet.Attributes
+open BenchmarkDotNet.Running
+
 open FastCfr
+
+type Benchmark() =
+
+    [<Params(50_000)>]
+    member val NumGames = 0 with get, set
+
+    [<Params(10, 100, 250, 1000)>]
+    member val ChunkSize = 0 with get, set
+
+    [<Benchmark>]
+    member this.LeducHoldem() =
+        LeducHoldem.train this.NumGames this.ChunkSize
 
 module Program =
 
     let run () =
 
             // train
-        let numGames = 50000
+        let numGames = 50_000
         let chunkSize = 250
         printfn $"Running Leduc Hold'em parallel CFR for {numGames} games"
         printfn $"Server garbage collection: {Runtime.GCSettings.IsServerGC}\n"
+        let timer = Diagnostics.Stopwatch.StartNew()
         let util, infoSetMap = LeducHoldem.train numGames chunkSize
 
             // expected overall utility
@@ -35,7 +52,7 @@ module Program =
                     |> String.concat ", "
             printfn $"%-11s{key}:    {str}"
 
-    let timer = Diagnostics.Stopwatch.StartNew()
-    run ()
-    printfn ""
-    printfn $"Elapsed time: {timer}"
+        printfn ""
+        printfn $"Elapsed time: {timer}"
+
+    BenchmarkRunner.Run(typeof<Benchmark>) |> ignore
