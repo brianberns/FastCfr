6a6157c95d2dcb262933c3b92444636c0f8dc259
Brian Berns <brianberns@gmail.com>
2024-10-24 08:57:57 -0400

    Added Kuhn Poker.

---
+++ FastCfr.Test/KuhnPoker.fs
@@ -0,0 +1,84 @@
+﻿/// Kuhn poker
+module KuhnPoker
+
+open FastCfr
+
+/// Number of players.
+let numPlayers = 2
+
+/// Available player actions.
+let actions =
+    [|
+        "b"   // bet/call
+        "c"   // check/fold
+    |]
+
+/// Cards in the deck.
+let deck =
+    [
+        "J"   // Jack
+        "Q"   // Queen
+        "K"   // King
+    ]
+
+/// Gets zero-based index of active player.
+let getActivePlayer (history : string) =
+    history.Length % numPlayers
+
+/// Gets payoff for the active player if the game is over.
+let getPayoff (cards : string[]) = function
+
+        // opponent folds - active player wins
+    | "bc" | "cbc" -> Some 1
+
+        // showdown
+    | "cc" | "bb" | "cbb" as history ->
+        let payoff =
+            if history.Contains('b') then 2 else 1
+        let activePlayer = getActivePlayer history
+        let playerCard = cards[activePlayer]
+        let opponentCard =
+            cards[(activePlayer + 1) % numPlayers]
+        match playerCard, opponentCard with
+            | "K", _
+            | _, "J" -> payoff   // active player wins
+            | _ -> -payoff       // opponent wins
+            |> Some
+
+        // game not over
+    | _ -> None
+
+let rec createGameState cards history =
+    let activePlayer = getActivePlayer history
+    match getPayoff cards history with
+        | Some payoff ->
+            TerminalGameState.create activePlayer payoff
+                |> Terminal
+        | None ->
+            let infoSetKey =
+                sprintf "%s%s" cards[activePlayer] history
+            NonTerminal {
+                ActivePlayerIdx = activePlayer
+                InfoSetKey = infoSetKey
+                LegalActions = actions
+                AddAction =
+                    fun action ->
+                        let history = history + action
+                        createGameState cards history
+            }
+
+let train numGames chunkSize =
+    let rng = System.Random(0)
+    let games =
+        [|
+            for c0 in deck do
+                for c1 in deck do
+                    if c0 <> c1 then
+                        yield createGameState [| c0; c1 |] ""
+        |]
+    let gameChunks =
+        Seq.initInfinite (fun _ -> rng.GetItems(games, 1))
+            |> Seq.concat
+            |> Seq.truncate numGames
+            |> Seq.chunkBySize chunkSize
+    Trainer.train gameChunks
--- FastCfr.Test/Tests.fs
+++ FastCfr.Test/Tests.fs
@@ -3,8 +3,15 @@ module Tests
 open Xunit
 
 [<Fact>]
+let ``Kuhn Poiker`` () =
+    let numGames = 50000
+    let chunkSize = 250
+    let util, _ = KuhnPoker.train numGames chunkSize
+    Assert.Equal(-1./18., util, 0.005)
+
+[<Fact>]
 let ``Leduc Hold'em`` () =
     let numGames = 50000
     let chunkSize = 250
     let util, _ = LeducHoldem.train numGames chunkSize
-    Assert.Equal(-0.08018, util, 0.000005)
+    Assert.Equal(-0.085, util, 0.005)
